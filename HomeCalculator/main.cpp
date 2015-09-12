74e2c03dea7e537b32836644e58a08a478f83c14
dimonzav <dmytrozaverukha@gmail.com>
2015-09-12 16:49:40 +0300

    Create function that get a current time

--- HomeCalculator/main.cpp
+++ HomeCalculator/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include "time.h"
 
 using namespace std;
 
+void getTime();
+
 int main(){
 
-	cout << "Hello world !!!" << endl;
+	getTime();
 
+	cout << "Hello world !!!" << endl;
+	
 	system("Pause");
 
 	return 0;
+}
+
+void getTime()
+{
+	char timeBuffer[32];
+	struct tm newTime;
+	__time32_t aclock;
+
+	errno_t errNum;
+	_time32(&aclock);
+	_localtime32_s(&newTime, &aclock);
+	errNum = asctime_s(timeBuffer, 32, &newTime);
+
+	cout << timeBuffer << endl;
 }
